e391e23243987f3d428fd78211f701fd1ec174b7
Laércio Silva <laerciossjr@gmail.com>
2017-10-10 11:04:14 -0300

    Preparação dos arquivos e script para subir a base

---
+++ BaseDeDados/CriarBase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -X PUT http://localhost:5984/hollywood
+curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Estudios.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Filmes.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Diretores.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Atores.json -H "Content-type: application/json"
