e55c76c618d1ec675b91999cb4a0d5aa2f16a06a
Laércio Silva <laerciossjr@gmail.com>
2017-11-04 21:46:51 -0200

    Adicionando Alunos, Instrutores, Pilotos

--- BaseDeDados/CriarBase.sh
+++ BaseDeDados/CriarBase.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-curl -X PUT http://localhost:5984/hollywood
-curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Usuarios.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Estudios.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Filmes.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Diretores.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/hollywood/_bulk_docs -d @Atores.json -H "Content-type: application/json"
+curl -X PUT http://localhost:5984/escola_pilotos
+#curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Usuarios.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Pilotos.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Instrutores.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Alunos.json -H "Content-type: application/json"
