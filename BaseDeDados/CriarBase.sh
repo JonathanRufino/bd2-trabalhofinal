f38709fe047e71d2c273a912abc6bfd45ab95478
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-13 20:35:13 -0200

    Atualizando estrutura da base de dados

--- BaseDeDados/CriarBase.sh
+++ BaseDeDados/CriarBase.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-curl -X PUT http://localhost:5984/escola_pilotos
-#curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Usuarios.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Pilotos.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Instrutores.json -H "Content-type: application/json"
-curl -X POST http://localhost:5984/escola_pilotos/_bulk_docs -d @Alunos.json -H "Content-type: application/json"
+
+echo "Criando base de dados..."
+curl -X PUT http://localhost:5984/aeroclube
+
+echo "Populando documentos..."
+curl -X POST http://localhost:5984/aeroclube/_bulk_docs -d @documentos/Usuarios.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/aeroclube/_bulk_docs -d @documentos/Pilotos.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/aeroclube/_bulk_docs -d @documentos/Instrutores.json -H "Content-type: application/json"
+curl -X POST http://localhost:5984/aeroclube/_bulk_docs -d @documentos/Alunos.json -H "Content-type: application/json"
+
+# echo "Criando views..."
+# curl -X PUT http://localhost:5984/aeroclube/_design/alunos/_view/listar_alunos_e_horas_voo -d @views/listar_alunos_e_horas_voo.js -H "Content-type: application/json"
+# curl -X PUT http://localhost:5984/aeroclube/_design/instrutores/_view/listar_instrutores -d @views/listar_instrutores.js -H "Content-type: application/json"
+# curl -X PUT http://localhost:5984/aeroclube/_design/instrutores/_view/listar_cursos_instrutor -d @views/listar_cursos_instrutor.js -H "Content-type: application/json"
+# curl -X PUT http://localhost:5984/aeroclube/_design/pilotos/_view/listar_pilotos -d @views/listar_pilotos.js -H "Content-type: application/json"
+
+echo "Finalizado."
